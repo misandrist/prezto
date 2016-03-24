ae566404d5a1cb8fa97d6a0ffac482b978c9c18b
Evan Cofsky <evan@theunixman.com>
2016-03-24 14:36:37 -0500

    Add an install script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    echo Installing $rcfile
+    (cd $HOME && ln -s "$rcfile" ".${rcfile:t}") || break
+done
